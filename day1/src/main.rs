257a4ddc01a5a3719c8808fa2b078a2088e4b056
Daniel Eros <erosd99@gmail.com>
2024-12-08 19:34:27 +0100

    day 1 part 2 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("data/input.txt").expect("Input file was not found!");
@@ -15,11 +15,27 @@ fn main() {
     left.sort();
     right.sort();
 
-    let sum: u32 = left
-        .into_iter()
-        .zip(right.into_iter())
-        .map(|(a, b)| a.abs_diff(b))
+    let sum_part_1: u32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(a, b)| a.abs_diff(*b))
         .sum();
 
-    println!("{}", sum);
+    println!("{}", sum_part_1);
+
+    let mut elements_with_count: HashMap<u32, u32> = HashMap::new();
+
+    for element in &right {
+        let count: u32 = u32::try_from(right.iter().filter(|x| *x == element).count())
+            .expect("Count is way too big");
+
+        elements_with_count.insert(*element, count);
+    }
+
+    let sum_part_2: u32 = left
+        .iter()
+        .map(|x| x * elements_with_count.get(x).unwrap_or(&0))
+        .sum();
+
+    println!("{}", sum_part_2);
 }
